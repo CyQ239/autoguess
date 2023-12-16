af390465fb9ebe31cf3dfe485d55a1e89c2fae62
CyQ239 <fyzb007@126.com>
2023-12-16 19:30:49 +0800

    Initial commit

---
+++ bivium_guess_10.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 autoguess.py --inputfile ciphers/Bivium/BiviumA/relationfile_biviuma_177clk_mg14_ms85.txt --solver sat --maxguess 14 --maxsteps 85
---
+++ skinny.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 autoguess.py --inputfile ciphers/SKINNY-TK1/relationfile_skinnytk1_11r_mg15_ms75.txt --solver sat --maxguess 15 --maxsteps 75
